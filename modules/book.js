305a7fde11d752ef5a7e3c0c17bdc72d6e0819f6
Munana Clinton <munanaclinton@gmail.com>
2023-05-01 20:26:25 +0200

    Add modules of javascript with book.js and luxon.js library

---
+++ modules/book.js
@@ -0,0 +1,27 @@
+export default class Book {
+  constructor() {
+    this.bookLists = JSON.parse(localStorage.getItem('books')) || [];
+  }
+
+  addBooktoList(author, title) {
+    const updateBook = [
+      ...this.bookLists,
+      { id: `${Math.trunc(Math.random() * 100000)}${author.split(' ')[0]}`, author, title },
+    ];
+    this.updateStorage(updateBook);
+  }
+
+  removeBookfromList(id) {
+    const updateBook = this.bookLists.filter((it) => it.id !== id);
+    this.updateStorage(updateBook);
+  }
+
+  getBook() {
+    return this.bookLists;
+  }
+
+  updateStorage(data) {
+    localStorage.setItem('books', JSON.stringify(data));
+    this.bookLists = data;
+  }
+}
